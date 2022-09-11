31ae73c07d8c8fca42bac663de9afc0fc77b0c80
Edgar Giovanni Vargas Ocampo <egiovanni.vo@gmail.com>
2022-09-10 19:36:48 -0500

    tailwind dist removed from gitignore

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,20 +7,5 @@ module.exports = {
       'mobile': {'max': '510px'}
     },
   },
-  plugins: [
-    'tailwindcss',
-    'postcss-flexbugs-fixes',
-    [
-      'postcss-preset-env',
-      {
-        autoprefixer: {
-          flexbox: 'no-2009',
-        },
-        stage: 3,
-        features: {
-          'custom-properties': false,
-        },
-      },
-    ],
-  ],
+  plugins: [],
 }
