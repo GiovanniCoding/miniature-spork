eb82a465b75106e383d90ef4fccf373ce070516c
Edgar Giovanni Vargas Ocampo <egiovanni.vo@gmail.com>
2022-09-10 19:22:26 -0500

    tailwind correction

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux'
 
 import store from './redux/store'
 
+import '../dist/output.css'
+
 ReactDOM.createRoot(
   document.getElementById('root')).render(
     <Provider store={store}>
