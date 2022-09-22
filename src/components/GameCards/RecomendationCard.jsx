92e18c1162e00b1f096f20b3562f419036334b76
Edgar Giovanni Vargas Ocampo <egiovanni.vo@gmail.com>
2022-09-22 02:31:24 -0500

    Connection model with Frontend

--- src/components/GameCards/RecomendationCard.jsx
+++ src/components/GameCards/RecomendationCard.jsx
@@ -1,17 +1,32 @@
-export const RecomendationCard = ( ) => {
+import { useEffect, useState } from "react"
+
+export const RecomendationCard = ( { card } ) => {
+
+  const [gameInfo, setGameInfo] = useState({})
+
+  useEffect(() => {
+    
+    fetch(
+      `http://api.egiovanni.com/db/?games=${ card }`
+    )
+      .then( response => response.json() )
+      .then( data => setGameInfo({ title: data[0][1], release_date: data[0][2], image: data[0][3], description: data[0][4] }) )
+
+  }, [ card ])
+
   return (
     <div className="min-w-[16rem] h-[24rem] bg-[#1182a4] rounded-md basis-[16rem] flex-grow">
       <figure className="h-[10rem] px-4 pb-2 pt-4">
         <img
-          src={'https://imgs.search.brave.com/WStuyaqp1ABullS8QtLmfNZVe2qQkHjmB1HOuQ3z5Pc/rs:fit:1200:720:1/g:ce/aHR0cHM6Ly93d3cu/bWFzZ2FtZXJzLmNv/bS93cC1jb250ZW50/L3VwbG9hZHMvMjAy/MC8wOS9tYXJpby1j/b2NoZXMtLmpwZw'}
-          alt='Mario Kart Live: Home Circuit'
+          src={ gameInfo.image }
+          alt={ gameInfo.title }
           className="w-full h-full object-cover rounded-lg"
         />
       </figure>
       <div className="h-[14rem]">
-        <h1 className="font-semibold text-[1.2rem] px-6 text-gray-50">Mario Kart Live: Home Circuit</h1>
-        <h2 className="italic text-xs text-gray-50 px-6">28th Apr 2017</h2>
-        <p className="px-6 pt-1 text-[0.9rem] text-justify text-gray-50 max-h-[9rem] overflow-hidden">Mario Kart Live: Home Circuit is a racing game for the Nintendo Switch. The game is an augmented reality Mario Kart game played using RC Karts controlled via the Switch. The race is set up four gates placed around the room with the path directed by driving the included kart. The game comes in two different sets, one including Mario, the other including Luigi. The game itself has to be downloaded from the eShop, but it only displays a video without the accessories.</p>
+        <h1 className="font-semibold text-[1.2rem] px-6 text-gray-50">{ gameInfo.title }</h1>
+        <h2 className="italic text-xs text-gray-50 px-6">{ gameInfo.release_date }</h2>
+        <p className="px-6 pt-1 text-[0.9rem] text-justify text-gray-50 max-h-[9rem] overflow-hidden">{ gameInfo.description }</p>
       </div>
     </div>
   )
--- src/components/RecomendationsGrid.jsx
+++ src/components/RecomendationsGrid.jsx
@@ -1,18 +1,27 @@
+import { useEffect, useState } from "react"
 import { useSelector } from "react-redux"
 import { RecomendationCard } from "./GameCards/RecomendationCard"
 
 export const RecomendationsGrid = () => {
-  const recommendedGames = useSelector( state => state.games.recommendedGames )
-  const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  const { id } = useSelector( state => state.games.favoriteGame )
+  const [recommendedGames, setRecommendedGames] = useState([])
+
+  useEffect(() => {
+    fetch(
+      `http://api.egiovanni.com/model/?id=${id}`
+    )
+      .then( response => response.json() )
+      .then( data => setRecommendedGames( data ) )
+  }, [ id ])
 
   return (
-    recommendedGames.lenght > 0
+    recommendedGames.length > 0
     &&
     <div className="max-w-[70rem] mx-auto mt-4 mb-8">
       <div className="mx-8 flex flex-wrap gap-y-4 gap-x-2 justify-evenly">
         {
-          data.map( card => {
-            return <RecomendationCard key={card} />
+          recommendedGames.map( card => {
+            return <RecomendationCard key={card} card={ card } />
           } )
         }
       </div>
