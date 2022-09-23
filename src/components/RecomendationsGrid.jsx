2bd5f437a4e7d60f2ba0b9555654cf48164ae719
Edgar Giovanni Vargas Ocampo <egiovanni.vo@gmail.com>
2022-09-22 23:34:14 -0500

    useRef removed

--- src/components/RecomendationsGrid.jsx
+++ src/components/RecomendationsGrid.jsx
@@ -5,7 +5,6 @@ import { RecomendationCard } from "./GameCards/RecomendationCard"
 export const RecomendationsGrid = () => {
   const { id } = useSelector( state => state.games.favoriteGame )
   const [recommendedGames, setRecommendedGames] = useState([])
-  const refRemoveKeyboard = useRef()
 
   useEffect(() => {
     if ( id != '') {
@@ -14,27 +13,21 @@ export const RecomendationsGrid = () => {
       )
         .then( response => response.json() )
         .then( data => setRecommendedGames( data ) )
-      console.log( refRemoveKeyboard )
-      refRemoveKeyboard.current.focus()
     }
     
   }, [ id ])
 
   return (
-    <div ref={ refRemoveKeyboard }>
-      {
-        recommendedGames.length > 0
-        &&
-        <div className="max-w-[70rem] mx-auto mt-4 mb-8">
-          <div className="mx-8 flex flex-wrap gap-y-4 gap-x-2 justify-evenly">
-            {
-              recommendedGames.map( card => {
-                return <RecomendationCard key={card} card={ card } />
-              } )
-            }
-          </div>
-        </div>
-      }
+    recommendedGames.length > 0
+    &&
+    <div className="max-w-[70rem] mx-auto mt-4 mb-8">
+      <div className="mx-8 flex flex-wrap gap-y-4 gap-x-2 justify-evenly">
+        {
+          recommendedGames.map( card => {
+            return <RecomendationCard key={card} card={ card } />
+          } )
+        }
+      </div>
     </div>
   )
 }
