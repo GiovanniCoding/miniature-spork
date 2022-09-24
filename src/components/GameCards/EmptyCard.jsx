62cf4bd439eaa54072386b6e6004fe801750c1e2
Edgar Giovanni Vargas Ocampo <egiovanni.vo@gmail.com>
2022-09-24 16:18:48 -0500

    Text to be more explicit

--- src/components/GameCards/EmptyCard.jsx
+++ src/components/GameCards/EmptyCard.jsx
@@ -3,7 +3,7 @@ export const EmptyCard = () => {
     <div className="max-w-[70rem] mx-auto">
         <div className="mx-8 flex border rounded-lg bg-[#006d77] mobile:flex-col">
             <div className="my-4 mx-auto mobile:m-4">
-                <p className="bg-[#138089] p-2 text-gray-50 rounded-md max-h-[8rem] overflow-hidden text-ce">Favor de seleccionar un juego...</p>
+                <p className="bg-[#138089] p-2 text-gray-50 rounded-md max-h-[8rem] overflow-hidden text-center">Please select a game to show you some recomendations...</p>
             </div>
         </div>
     </div>
--- src/pages/Recomendations.jsx
+++ src/pages/Recomendations.jsx
@@ -4,7 +4,9 @@ import { RecomendationsGrid } from "../components/RecomendationsGrid"
 export const Recomendations = () => {
   return (
     <>
+      <h1 className="flex justify-center my-1 text-gray-500">You selected the game:</h1>
       <MainGameCard />
+      <h2 className="flex justify-center my-1 text-gray-500">Some games you might like:</h2>
       <RecomendationsGrid />
     </>
   )
